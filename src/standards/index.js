b798b47efbac6d6f52433fbcaf3cbfa8c6a74e6a
MBorne <mickael.borne@ign.fr>
2021-07-15 19:47:21 +0200

    update

--- src/standards/index.js
+++ src/standards/index.js
@@ -1,6 +1,4 @@
 import expStandards from './exp-standards.json';
 import gpuStandards from './gpu-standards.json';
-
 const standards = [...expStandards,...gpuStandards];
-
 export default standards;
